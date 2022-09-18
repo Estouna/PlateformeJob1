15eff190c62a5b22ea41ee5e4335ad36adebbdcb
Estouna <cyril.depont@yahoo.fr>
2022-09-18 19:08:56 +0200

    18/09/2022

---
+++ calculator9000/src/backend/db.php
@@ -0,0 +1,8 @@
+<?php
+// try {
+//     $db = new PDO('mysql:host=localhost;dbname=backup_calculator9000;charset=utf8;', 'root', '');
+//     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// } catch (Exception $e) {
+//     echo "Database Error : " . $e->getMessage();
+// }
